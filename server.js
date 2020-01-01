4ddf2475c537f3e6089db94aa71e61589433b5f3
Joaquim <pgrilo92@hotmail.com>
2020-01-01 18:08:01 -0500

    add ajax

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var usersRouter = require('./routes/users');
 // let teamsRouter = require('./routes/teams')
 // let playersRouter = require('./routes/players')
 
-require('dotenv').config()
+//require('dotenv').config()
 
 var app = express();
 
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+  //res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
