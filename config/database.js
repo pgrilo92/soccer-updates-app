8b6821e904bd5d44598002421859c48b6cb902bc
Joaquim <pgrilo92@hotmail.com>
2020-01-10 10:41:48 -0500

    connect database

--- config/database.js
+++ config/database.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://pgrilo92:<camera92>@soccer-database-vwsyo.mongodb.net/test?retryWrites=true&w=majority', {
+const uri = process.env.MONGODB_URL
+mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 })
-// mongoose.connect('mongodb://localhost/soccerupdatesdb', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true
-// })
+
+const db = mongoose.connection
+
+db.on('connected', () => {
+    console.log(`Mongoose is connected ${db.host}:${db.port}`)
+})
